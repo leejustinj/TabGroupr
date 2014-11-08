b40738e7d275f36e2c4c4e04191dacb60c31337f
Justin Lee <justinlee1995@gmail.com>
2014-11-07 22:36:09 -0500

    Added JQuery code to display default group and show/hide tabs

--- popup.js
+++ popup.js
@@ -1,1 +1,34 @@
 
+var groups = [];
+
+var defaultGroup = {
+  name:"Default Group",
+  tabs:[],
+  open:false
+}
+
+$(document).ready(function(){
+  chrome.tabs.query({currentWindow:true},function(tabarray){
+    defaultGroup.tabs = tabarray;
+    groups.push(defaultGroup);
+    for(var i = 0;i<groups.length;i++){
+      $(".container").append("<div class='group group"+i+"'><a href='#'><h3>"+groups[i].name+"</h3></a></div>");
+      
+      $(".group"+i).click({groupnum:i},function(event){
+	var i = event.data.groupnum;
+	if(groups[i].open===false){
+ 	  groups[i].open=true;
+	  for(var j = 0;j<groups[i].tabs.length;j++){
+	    $(".group"+i).append("<div class='tab"+i+"'><h5>"+groups[i].tabs[j].title+"</h5></div>");
+	  }
+	}
+	else{
+	  groups[i].open=false;
+	  $(".tab"+i).remove();
+	}
+      });
+      
+    }
+  });
+  
+});
