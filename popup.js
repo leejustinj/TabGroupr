48055387fb3296146cd00ff232f42cd12e79256d
Jesse Anderson <jesseranders@gmail.com>
2014-11-09 10:16:08 -0800

    added moveTab function

--- popup.js
+++ popup.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
   chrome.tabs.query({currentWindow:true},function(tabarray){
     defaultGroup.tabs = tabarray;
     groups.push(defaultGroup);
-    for(var i = 0;i<groups.length;i++){
+      for(var i = 0;i<groups.length;i++){
       $(".container").append("<div class='group group"+i+"'><a href='#'><h3>"+groups[i].name+"</h3></a></div>");
       
       $(".group"+i).click({groupnum:i},function(event){
@@ -29,6 +29,35 @@ $(document).ready(function(){
       });
       
     }
-  });
-  
+  });  
 });
+
+// IMPORTANT:  this could be totally wrong.
+// I have no idea how to test.  Plz help.
+//
+// Intended to move tab from one group to another
+// If the source or dest group is open, modifies tab state
+// Does nothing with the UI at this time 
+function moveTab(tabID, destGroupID){
+  var destGroup, sourceGroup, tab;
+  for(var i = 0; i<groups.length;i++){
+    if(groups[i].id===destGroupID){
+      destGroup = groups[i];
+    }
+    for(var j = 0; j<groups[i].tabs.length;j++){
+      if(groups[i].tabs[j].id===tabID){
+        tab = groups[i].tabs[j];
+        sourceGroup = groups[i];
+        sourceGroup.tabs.splice(j,1);
+        break;
+      }
+    } 
+    destGroup.tabs.push(tab);
+    if(sourceGroup.open){
+      chrome.tabs.remove(tabID);
+    }
+    if(destGroup.open){
+      chrome.tabs.create({url:tab.url,selected:false});
+    }
+  }
+}
